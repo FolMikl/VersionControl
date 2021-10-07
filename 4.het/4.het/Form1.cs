d683bd8f1362c352a3e5fb0062cf7f0b823613cd
FolMikl <foldiak.miklos@gmail.com>
2021-10-07 13:19:57 +0200

    4.feladat

--- 4.het/4.het/Form1.cs
+++ 4.het/4.het/Form1.cs
@@ -12,9 +12,16 @@ namespace _4.het
 {
     public partial class Form1 : Form
     {
+        RealEstateEntities context = new RealEstateEntities();
+        List<Flat> Flats;
         public Form1()
         {
             InitializeComponent();
+            LoadData();
+        }
+        private void LoadData()
+        {
+            Flats = context.Flat.ToList();
         }
     }
 }
