a24651d7d614b6b65711eaf479a8cc2612b0f7b9
FolMikl <foldiak.miklos@gmail.com>
2021-11-12 18:36:55 +0100

    kisautó

--- 7.feladat/7.feladat/Car.cs
+++ 7.feladat/7.feladat/Car.cs
@@ -7,11 +7,12 @@ using System.Threading.Tasks;
 
 namespace _7.feladat
 {
-    class Car : Toy
+    public class Car : Toy
     {
         protected override void DrawImage(Graphics g)
         {
-            throw new NotImplementedException();
+            Image imageFile = Image.FromFile("Images/car.png");
+            g.DrawImage(imageFile, new Rectangle(0, 0, Width, Height));
         }
     }
 }
