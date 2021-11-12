70960d00389c152f1a7222664250d621dde9c6e8
FolMikl <foldiak.miklos@gmail.com>
2021-11-12 13:41:59 +0100

    kezdes

---
+++ 7.feladat/7.feladat/Ball.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _7.feladat
+{
+    public class Ball : Label
+    {
+        public Ball()
+        {
+            AutoSize = false;
+            Width = 50;
+            Height = Width;
+        }
+        private void Ball_Paint(object sender, PaintEventArgs e)
+        {
+            DrawImage(e.Graphics);
+        }
+        protected void DrawImage(Graphics g)
+        {
+            g.FillEllipse(new SolidBrush(Color.Blue), 0, 0, Width, Height);
+        }
+        public void MoveBall()
+        {
+            Left += 1;
+        }
+
+    }
+
+}
