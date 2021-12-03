6050bd5e268ad24bdc65f53297c7e68989fcf807
FolMikl <foldiak.miklos@gmail.com>
2021-12-03 12:36:19 +0100

    osztályok

---
+++ 9.het_szimulacio/9.het_szimulacio/BirthProbability.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _9.het_szimulacio
+{
+    class BirthProbability
+    {
+        public double P { get; set; }
+    }
+}
